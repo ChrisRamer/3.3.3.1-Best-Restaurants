31a50ec96aece301731d5ac9a9b2f0255b0523ed
Chris Ramer <chrisramer.97@gmail.com>
2021-03-30 09:48:44 -0700

    Use primary keys

--- BestRestaurants.Solution/BestRestaurants/Models/Cuisine.cs
+++ BestRestaurants.Solution/BestRestaurants/Models/Cuisine.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BestRestaurants.Models
 {
 	public class Cuisine
 	{
+		[Key]
 		public string Name { get; set; }
 		public int Price { get; set; }
 		public virtual Restaurant Restaurant { get; set; }
--- BestRestaurants.Solution/BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants.Solution/BestRestaurants/Models/Restaurant.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BestRestaurants.Models
 {
 	public class Restaurant
 	{
+		[Key]
 		public string Name { get; set; }
 		public int Rating { get; set; }
 		public virtual ICollection<Cuisine> Cuisine { get; set; }
